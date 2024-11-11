a6353f2729792314917c873f1e4b5dc28a81d035
olaoluwajo <olagundoyealexander@gmail.com>
2024-11-11 03:32:05 +0100

    create signup api route

--- api/index.js
+++ api/index.js
@@ -1,6 +1,8 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRoutes from "./routes/user.route.js";
+import authRoutes from "./routes/auth.route.js";
 dotenv.config();
 
 mongoose
@@ -10,10 +12,11 @@ mongoose
 
 const app = express();
 
+app.use(express.json());
+
 app.listen(3000, () => {
 	console.log("Server is running on port 3000");
 });
 
-app.get("/", (req, res) => {
-	res.send("Hello, World!");
-});
+app.use("/api/user", userRoutes);
+app.use("/api/auth", authRoutes);
