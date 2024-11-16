ccdf7639cb1eec5c6a485ded62693d76a7aa60bc
olaoluwajo <olagundoyealexander@gmail.com>
2024-11-16 04:38:11 +0100

    privateroute added

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,8 +1,8 @@
 import { Link } from "react-router-dom";
-// import { useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 
 export default function Header() {
-	// const { currentUser } = useSelector((state) => state.user);
+	const { currentUser } = useSelector((state) => state.user);
 	return (
 		<div className="bg-slate-200">
 			<div className="flex justify-between items-center max-w-6xl mx-auto p-3">
@@ -16,10 +16,10 @@ export default function Header() {
 					<Link to="/about">
 						<li>About</li>
 					</Link>
-					<Link to="/profile">
+					{/* <Link to="/profile">
 						<li>Sign In</li>
-					</Link>
-					{/* <Link to="/profile">{currentUser ? <img src={currentUser.profilePicture} alt="profile" className="h-7 w-7 rounded-full object-cover" /> : <li>Sign In</li>}</Link> */}
+					</Link> */}
+					<Link to="/profile">{currentUser ? <img src={currentUser.profilePicture} alt="profile" className="size-7	 rounded-full object-cover" /> : <li>Sign In</li>}</Link>
 				</ul>
 			</div>
 		</div>
